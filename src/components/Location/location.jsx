21accbd69e85bf0562135687457e968ed063e8cc
saidmuhammadali <saidsaidakbarov2009@gmail.com>
2023-10-21 21:09:10 +0500

    first-project

--- src/components/Location/location.jsx
+++ src/components/Location/location.jsx
@@ -3,7 +3,8 @@ import "leaflet/dist/leaflet.css";
 import "../Location/style.css";
 import { Icon } from "leaflet";
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
-
+import ic from "../../img/ic.png";
+import lo from "../../img/lo.png";
 function Location() {
   const markers = [
     {
@@ -28,21 +29,26 @@ function Location() {
       <div className="h1div">
         <h2>GreenClean Location</h2>
       </div>
-      <div className="roww">
-        <div className="divv">
-          <MapContainer center={[41.311594, 69.298936]} zoom={10}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+      <div className="divv">
+        <MapContainer center={[41.311594, 69.298936]} zoom={10}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-            {markers.map((markers) => {
-              return <Marker position={markers.getcode} icon={customIcon} />;
-            })}
-          </MapContainer>
-          <div>
-            <h1></h1>
-          </div>
+          {markers.map((markers) => {
+            return <Marker position={markers.getcode} icon={customIcon} />;
+          })}
+        </MapContainer>
+        <div className="asaa">
+          <h2>GreenClean City</h2>
+        </div>
+        <div className="as">
+          <img
+            src={ic}
+            style={{ width: "35px", height: "35px", marginTop: "50px" }}
+            alt=""
+          />
         </div>
       </div>
     </>
