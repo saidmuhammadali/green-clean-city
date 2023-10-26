c51f5be16f75d60e49232dd7253b31185e982704
saidmuhammadali <saidsaidakbarov2009@gmail.com>
2023-10-26 15:18:45 +0500

    first-project

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import { Bgn, Csa, HeaderDiv, HeaderImg, Img } from "./style";
 import city from "../../img/city.png";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import "./stil.css";
 function Header() {
   const navigate = useNavigate();
   return (
@@ -28,6 +29,44 @@ function Header() {
           </HeaderImg>
         </HeaderDiv>
       </Csa>
+      <div className="Footer">
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-md-3 col-lg-3 col-12 ft-2"
+              style={{ marginTop: "-70px" }}
+            >
+              <h5>Quick Links</h5>
+              <ul>
+                <li className="nav-item">
+                  <a className="" href="/home">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/Donation">
+                    Donation
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/marketplace">
+                    {" "}
+                    MarketPlace
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/location">
+                    Location
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Last-footer">
+        <p>Design By Green Clean City</p>
+      </div>
     </>
   );
 }
