68d620ac53540a7c00dfa74b0bef1cdaa3db6ca9
saidmuhammadali <saidsaidakbarov2009@gmail.com>
2023-10-26 15:50:48 +0500

    first-project

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,7 +14,7 @@ function Header() {
             fontFamily: "Anton",
             color: "wheat",
             marginLeft: "125px",
-            marginTop: "90px",
+            marginTop: "110px",
           }}
           animate={{ opacity: 1 }}
           initial={{ opacity: 0 }}
@@ -36,26 +36,30 @@ function Header() {
               className="col-md-3 col-lg-3 col-12 ft-2"
               style={{ marginTop: "-70px" }}
             >
-              <h5>Quick Links</h5>
+              <h5 style={{ fontSize: "15px" }}>Quick Links</h5>
               <ul>
                 <li className="nav-item">
-                  <a className="" href="/home">
+                  <a className="" href="/home" style={{ fontSize: "13px" }}>
                     Home
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="" href="/Donation">
+                  <a className="" href="/Donation" style={{ fontSize: "13px" }}>
                     Donation
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="" href="/marketplace">
+                  <a
+                    className=""
+                    href="/marketplace"
+                    style={{ fontSize: "13px" }}
+                  >
                     {" "}
                     MarketPlace
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="" href="/location">
+                  <a className="" href="/location" style={{ fontSize: "13px" }}>
                     Location
                   </a>
                 </li>
@@ -65,7 +69,7 @@ function Header() {
         </div>
       </div>
       <div className="Last-footer">
-        <p>Design By Green Clean City</p>
+        <p style={{ fontSize: "17px" }}>Design By Green Clean City</p>
       </div>
     </>
   );
--- src/components/Marketplace/index.jsx
+++ src/components/Marketplace/index.jsx
@@ -6,6 +6,7 @@ import { LuGrape } from "react-icons/lu";
 import { BiSolidBookAlt } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import bg from "../../img/marketbg.png";
+import "./stil.css";
 function MarketPlace() {
   const navigate = useNavigate();
   return (
@@ -41,6 +42,48 @@ function MarketPlace() {
       <Roww>
         <Da src={bg} alt="" />
       </Roww>
+      <div className="Footer">
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-md-3 col-lg-3 col-12 ft-2"
+              style={{ marginTop: "-70px" }}
+            >
+              <h5 style={{ fontSize: "15px" }}>Quick Links</h5>
+              <ul>
+                <li className="nav-item">
+                  <a className="" href="/home" style={{ fontSize: "13px" }}>
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/Donation" style={{ fontSize: "13px" }}>
+                    Donation
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className=""
+                    href="/marketplace"
+                    style={{ fontSize: "13px" }}
+                  >
+                    {" "}
+                    MarketPlace
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/location" style={{ fontSize: "13px" }}>
+                    Location
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Last-footer">
+        <p style={{ fontSize: "17px" }}>Design By Green Clean City</p>
+      </div>
     </>
   );
 }
