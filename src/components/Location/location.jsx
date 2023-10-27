9860dd837d587c17113f7ae23f21f0cc6b63e20b
saidmuhammadali <saidsaidakbarov2009@gmail.com>
2023-10-27 14:07:33 +0500

    first-project

--- src/components/Location/location.jsx
+++ src/components/Location/location.jsx
@@ -4,14 +4,9 @@ import "../Location/style.css";
 import { Icon } from "leaflet";
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
-import { AiFillInstagram } from "react-icons/ai";
-import { AiFillTwitterCircle } from "react-icons/ai";
-import { BsTelegram } from "react-icons/bs";
+import logo from "../../img/logo.png";
 function Location() {
   const navigate = useNavigate();
-  const instagramUrl = "https://www.instagram.com/team_1he_bests/";
-  const twitterUrl = "https://www.instagram.com/team_1he_bests/";
-  const telegramUrl = "https://www.instagram.com/team_1he_bests/";
   const markers = [
     {
       getcode: [41.314672, 69.273304],
@@ -57,6 +52,68 @@ function Location() {
             })}
           </MapContainer>
         </div>
+      </div>
+      <div className="Footer">
+        <div className="container">
+          <div className="row">
+            <div
+              className="col-md-3 col-lg-3 col-12 ft-2"
+              style={{ marginTop: "-80px" }}
+            >
+              <h5 style={{ fontSize: "15px" }}>Quick Links</h5>
+              <ul>
+                <li className="nav-item">
+                  <a className="" href="/home" style={{ fontSize: "11px" }}>
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/Donation" style={{ fontSize: "11px" }}>
+                    Donation
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className=""
+                    href="/marketplace"
+                    style={{ fontSize: "11px" }}
+                  >
+                    {" "}
+                    MarketPlace
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/location" style={{ fontSize: "11px" }}>
+                    Location
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <img
+              src={logo}
+              alt=""
+              style={{
+                width: "50px",
+                height: "30px",
+                marginTop: "-342px",
+                marginLeft: "30px",
+              }}
+            />
+            <p className="pp">
+              Lorem ipsum dolor sit <br /> amet consectetur,
+              <br /> adipisicing elit.
+            </p>
+          </div>
+          <div className="divbuton">
+            <input className="input" type="Email" placeholder="  Email" />
+            <button className="buttom">Subscribe</button>
+          </div>
+        </div>
+      </div>
+      <div className="Last-footer">
+        <p style={{ fontSize: "10px" }}>Design By Green Clean City</p>
       </div>
     </>
   );
